b75ee4a900624a35425cdc4e434c3a9f19386a65
Jose Arturo Pedraza Bonilla <arturo.pedraza@infomedia.com.mx>
2025-02-16 19:40:38 -0600

    commit inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    const sections = document.querySelectorAll(".seccion");
+
+    const showSections = () => {
+        const triggerBottom = window.innerHeight * 0.8;
+
+        sections.forEach(section => {
+            const sectionTop = section.getBoundingClientRect().top;
+
+            if(sectionTop < triggerBottom){
+                section.classList.add("show");            
+            }
+        });
+    };
+
+    window.addEventListener("scroll", showSections);
+    showSections();
+})
