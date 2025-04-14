5b85ccadfa19fa207ee064dee3ccecbf7697032f
Jose Arturo Pedraza Bonilla <arturo.pedraza@infomedia.com.mx>
2025-04-14 10:18:39 -0600

    fix: Se redefine la estructura de proyecto, se agregan imagenes de galerias y se definen funcionalidades nuevas para el manejo de las secciones

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-
-document.addEventListener("DOMContentLoaded", function(){
+/**
+ * Funcionalidad para llevar a cabo la animacion de 
+ * mostrar la pagina seccion con seccion
+ */
+document.addEventListener("DOMContentLoaded", function() {
     const sections = document.querySelectorAll(".seccion");
-
     const showSections = () => {
         const triggerBottom = window.innerHeight * 0.8;
 
         sections.forEach(section => {
             const sectionTop = section.getBoundingClientRect().top;
 
-            if(sectionTop < triggerBottom){
-                section.classList.add("show");            
+            if (sectionTop < triggerBottom) {
+                section.classList.add("show");
             }
         });
     };
 
     window.addEventListener("scroll", showSections);
     showSections();
-})
+});
